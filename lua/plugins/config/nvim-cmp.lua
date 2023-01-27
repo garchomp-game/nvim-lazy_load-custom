566cf9010a69460cec24c0162003d04f43476905
garchomp-game <otoka.japanesedrum@gmail.com>
2023-01-27 17:36:26 +0900

    commit

--- lua/plugins/config/nvim-cmp.lua
+++ lua/plugins/config/nvim-cmp.lua
@@ -5,9 +5,7 @@ cmp.setup({
 	snippet = {
 		-- REQUIRED - you must specify a snippet engine
 		expand = function(args)
-			vim.fn["vsnip#anonymous"](args.body) -- For `vsnip` users.
 			require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
-			require('snippy').expand_snippet(args.body) -- For `snippy` users.
 		end,
 	},
 	window = {
@@ -22,10 +20,7 @@ cmp.setup({
 		['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
 	}),
 	sources = cmp.config.sources({
-		{ name = 'nvim_lsp' },
-		{ name = 'vsnip' }, -- For vsnip users.
 		{ name = 'luasnip' }, -- For luasnip users.
-		{ name = 'snippy' }, -- For snippy users.
 	}, {
 		{ name = 'buffer' },
 	})
