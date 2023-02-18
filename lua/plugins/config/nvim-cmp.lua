9356bffd896a3b43e4525a52eda29b76e8cc34ce
garchomp-game <otoka.japanesedrum@gmail.com>
2023-02-18 14:53:39 +0900

    fix

--- lua/plugins/config/nvim-cmp.lua
+++ lua/plugins/config/nvim-cmp.lua
@@ -18,30 +18,20 @@ cmp.setup({
     ['<C-Space>'] = cmp.mapping.complete(),
     ['<C-e>'] = cmp.mapping.abort(),
     ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
-    ["<Tab>"] = cmp.mapping(function(fallback)
+    ['<Tab>'] = cmp.mapping(function()
       if cmp.visible() then
         cmp.select_next_item()
-      elseif require("luasnip").expand_or_jumpable() then
-        vim.fn.feedkeys(vim.api.nvim_replace_termcodes("<Plug>luasnip-expand-or-jump", true, true, true), "")
       else
-        fallback()
+        feedkeys.call(keymap.t('<C-z>'), 'n')
       end
-    end, {
-        "i",
-        "s",
-      }),
-    ["<S-Tab>"] = cmp.mapping(function(fallback)
+    end),
+    ['<S-Tab>'] = cmp.mapping(function()
       if cmp.visible() then
         cmp.select_prev_item()
-      elseif require("luasnip").jumpable(-1) then
-        vim.fn.feedkeys(vim.api.nvim_replace_termcodes("<Plug>luasnip-jump-prev", true, true, true), "")
       else
-        fallback()
+        feedkeys.call(keymap.t('<C-z>'), 'n')
       end
-    end, {
-        "i",
-        "s",
-      }),
+    end),
   }),
   sources = cmp.config.sources({
     { name = 'nvim_lsp' },
