6baaf9189550ee8c3dde9643efcaf15980d72788
garchomp-game <otoka.japanesedrum@gmail.com>
2023-07-25 10:36:17 +0900

    disable java and lombok

--- init.lua
+++ init.lua
@@ -6,14 +6,14 @@ local plugins = require('plugins.init')
 local configs = require('config.lazyConfigs')
 
 local home = vim.env.HOME
-local lombok = "/.local/share/nvim/mason/packages/jdtls/lombok.jar"
-local viminfo = "mysession.viminfo"
+-- local lombok = "/.local/share/nvim/mason/packages/jdtls/lombok.jar"
+-- local viminfo = "mysession.viminfo"
 
-if vim.fn.filereadable(home .. lombok) then
-  vim.env.JDTLS_JVM_ARGS="-javaagent:"
-  .. home
-  .. lombok
-end
+-- if vim.fn.filereadable(home .. lombok) then
+--   vim.env.JDTLS_JVM_ARGS="-javaagent:"
+--   .. home
+--   .. lombok
+-- end
 
 if not vim.loop.fs_stat(lazypath) then
   vim.fn.system({
