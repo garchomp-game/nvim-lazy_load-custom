37d42eb3290fb50334aca60bd846382642dba8fc
garchomp-game <otoka.japanesedrum@gmail.com>
2023-08-07 14:35:15 +0900

    fix nvim-cmp

--- lua/plugins/nvim-cmp.lua
+++ lua/plugins/nvim-cmp.lua
@@ -19,14 +19,8 @@ return {
   {
     'hrsh7th/nvim-cmp',
     event = { 'CmdLineEnter', 'BufRead' },
-    dependencies = {
-      'L3MON4D3/LuaSnip',
-      'saadparwaiz1/cmp_luasnip'
-    },
     opts = function()
-
       -- Set up nvim-cmp.
-
       local has_words_before = function()
         local line, col = unpack(vim.api.nvim_win_get_cursor(0))
         return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
