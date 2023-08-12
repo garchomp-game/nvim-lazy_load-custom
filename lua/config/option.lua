ce205dc04090c5270717b2b19837c0eab2c4a1a1
garchomp-game <otoka.japanesedrum@gmail.com>
2023-08-12 23:16:21 +0900

    commit

--- lua/config/option.lua
+++ lua/config/option.lua
@@ -1,11 +1,9 @@
-vim.cmd([[colorscheme base16-ayu-dark]])
+vim.cmd([[colorscheme ayu]])
 vim.opt.helplang='ja,en'
 vim.opt.expandtab = true
 vim.opt.tabstop = 2
 vim.opt.shiftwidth =2
-vim.opt.number = false
 vim.opt.mouse = ""
-vim.opt.cursorline = false
 vim.opt.relativenumber = false
 vim.opt.cursorcolumn = false
 vim.opt.showcmd = true
