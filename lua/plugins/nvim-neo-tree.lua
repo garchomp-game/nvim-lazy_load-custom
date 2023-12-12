eae54489ff67a93798bb3968ec84ff09ab0acbef
garchomp-game <otoka.japanesedrum@gmail.com>
2023-12-12 15:19:32 +0900

    commit

--- lua/plugins/nvim-neo-tree.lua
+++ lua/plugins/nvim-neo-tree.lua
@@ -14,18 +14,6 @@ return {
     event = { 'CmdLineEnter', 'BufRead' },
     opts = function()
       require("neo-tree").setup({
-        -- その他の設定...
-        filesystem = {
-
-          components = {
-            size = false
-          },
-        },
-        window = {
-          auto_expand_width = true, -- ウィンドウ幅を自動的に拡張する
-          -- その他のウィンドウ関連の設定...
-        },
-        -- その他の設定...
       })
     end
   }
