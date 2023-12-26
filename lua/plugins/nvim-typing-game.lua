018c76abf3ddb65ff8ae2373df6b8ea771f105f9
garchomp-game <otoka.japanesedrum@gmail.com>
2023-12-27 00:03:32 +0900

    commit

--- lua/plugins/nvim-typing-game.lua
+++ lua/plugins/nvim-typing-game.lua
@@ -2,4 +2,13 @@ return {
   'garchomp-game/nvim-typing-game',
   dev = "true",
   event = { 'CmdLineEnter', 'BufRead' },
+  keys = { '<leader>t' },
+  config = function()
+    vim.api.nvim_set_keymap(
+      'n',
+      '<leader>t',
+      ":Lazy reload nvim-typing-game<CR>:MyFirstFunction<CR>",
+      { noremap = true, silent = true }
+    )
+  end,
 }
