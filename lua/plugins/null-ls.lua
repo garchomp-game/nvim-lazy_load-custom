7650e65ed7648535b604e7341c1096b316606681
garchomp-game <otoka.japanesedrum@gmail.com>
2024-01-05 10:02:51 +0900

    commit

--- lua/plugins/null-ls.lua
+++ lua/plugins/null-ls.lua
@@ -7,12 +7,12 @@ return {
   opts = function()
     local null_ls = require("null-ls")
     -- `null-ls`のセットアップ
-    null_ls.setup {
+    null_ls.setup({
       sources = {
         null_ls.builtins.diagnostics.markdownlint_cli2.with {
           args = { "$FILENAME" },
         },
       },
-    }
+    })
   end,
 }
