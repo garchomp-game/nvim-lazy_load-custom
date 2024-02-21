a53b666785b09b5da9ad8f3db07792bb5bf02a09
garchomp-game <otoka.japanesedrum@gmail.com>
2024-02-21 21:31:18 +0900

    commit

--- lua/plugins/codeium.lua
+++ lua/plugins/codeium.lua
@@ -1,90 +1,27 @@
-local M = {
-  {
-    'Exafunction/codeium.vim',
-    event = "VeryLazy",
-    keys = {
-      '<C-g>',
-      '<c-;>',
-      '<c-,>',
-      '<c-x>',
-      '<c-.>',
-      '<c-y>',
-      '<c-q>',
-      '<c-s>',
-    },
-    dependencies = { 'nvim-lua/plenary.nvim' },
-    config = function ()
-      -- Change '<C-g>' here to any keycode you like.
-      vim.keymap.set(
-        'i',
-        '<C-g>',
-        function ()
-          return vim.fn['codeium#Accept']()
-        end,
-        { expr = true, silent = true }
-      )
-      vim.keymap.set(
-        'i',
-        '<c-;>',
-        function()
-          return vim.fn['codeium#CycleCompletions'](1)
-        end,
-        { expr = true, silent = true }
-      )
-      vim.keymap.set(
-        'i',
-        '<c-,>',
-        function()
-          return vim.fn['codeium#CycleCompletions'](-1)
-        end,
-        { expr = true, silent = true }
-      )
-      vim.keymap.set(
-        'i',
-        '<c-x>',
-        function()
-          return vim.fn['codeium#Clear']()
-        end,
-        { expr = true, silent = true }
-      )
-      vim.keymap.set(
-        'i',
-        '<c-.>',
-        function()
-          return vim.fn['codeium#Complete']()
-        end,
-        { expr = true, silent = true }
-      )
-      vim.keymap.set(
-        'i',
-        '<c-y>',
-        function()
-          return vim.fn['codeium#YankKey']()
-        end,
-        { expr = true, silent = true }
-      )
-      vim.keymap.set(
-        'i',
-        '<c-q>',
-        function()
-          return vim.fn['codeium#Dismiss']()
-        end,
-        { expr = true, silent = true }
-      )
-      vim.keymap.set(
-        'i',
-        '<c-s>',
-        function()
-          return vim.fn['codeium#Select']()
-        end,
-        { expr = true, silent = true }
-      )
-    end
-  }
+return {
+	"Exafunction/codeium.vim",
+	event = "VeryLazy",
+	config = function()
+		-- Change '<C-g>' here to any keycode you like.
+		vim.keymap.set("i", "<C-g>g", function()
+			return vim.fn["codeium#Accept"]()
+		end, { expr = true, silent = true })
+		vim.keymap.set("i", "<c-g>;", function()
+			return vim.fn["codeium#CycleCompletions"](1)
+		end, { expr = true, silent = true })
+		vim.keymap.set("i", "<c-g>,", function()
+			return vim.fn["codeium#CycleCompletions"](-1)
+		end, { expr = true, silent = true })
+		vim.keymap.set("i", "<c-g>x", function()
+			return vim.fn["codeium#Clear"]()
+		end, { expr = true, silent = true })
+    vim.g.codeium_filetypes = {
+      markdown = false,
+      gitcommit = false,
+      gitrebase = false,
+      hgcommit = false,
+      svn = false,
+      cvs = false,
+    }
+	end,
 }
-local utils = require('utils')
-if utils.is_termux then
-  return {}
-else
-  return M
-end
--- lua/plugins/nvim-neo-tree.lua
+++ lua/plugins/nvim-neo-tree.lua
@@ -1,17 +1,23 @@
 return {
   'nvim-neo-tree/neo-tree.nvim',
-  event = "VeryLazy",
   keys = {'<C-n>'},
   dependencies = {
     'nvim-tree/nvim-web-devicons',
     "nvim-lua/plenary.nvim",
     "MunifTanjim/nui.nvim",
   },
-  config = function()
-    vim.api.nvim_set_keymap(
+  config = function(_, opts)
+    vim.keymap.set(
       'n','<C-n>',
       ':Neotree toggle<CR>',
       { noremap = true, silent = true }
     )
+    require("neo-tree").setup(opts)
   end,
+  opts = {
+    window = {
+      width = 25,
+      auto_expand_width = true,
+    }
+  }
 }
