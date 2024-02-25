5330e0a3b9791062dfafe70f8f9e0e62a44b7943
garchomp-game <otoka.japanesedrum@gmail.com>
2024-02-25 15:21:24 +0900

    commit

--- lua/config/command.lua
+++ lua/config/command.lua
@@ -8,13 +8,11 @@ vim.api.nvim_create_user_command(
 vim.api.nvim_create_user_command(
   'TrimTrailingSpace',
   function(opts)
-    -- 選択範囲を取得
     local start_line = opts.line1
     local end_line = opts.line2
-    -- 選択範囲内での末尾のスペースを削除するExコマンドを構築
-    local command = start_line .. "," .. end_line .. "s/\\s\\+$//g"
-    -- コマンドを実行
+    local command = start_line .. "," .. end_line .. "s/\\s\\+$//ge"
     vim.cmd(command)
+    vim.cmd("noh")
   end,
-  {range = true} -- コマンドが範囲を受け取れるようにする
+  {range = true}
 )
