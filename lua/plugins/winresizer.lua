e4a3d351f5e2d5a0db1057640b36c62605172e7b
garchomp-game <otoka.japanesedrum@gmail.com>
2024-02-26 14:54:49 +0900

    commit

--- lua/plugins/winresizer.lua
+++ lua/plugins/winresizer.lua
@@ -3,5 +3,7 @@ return {
   event = "VeryLazy",
   init = function()
     vim.g.winresizer_start_key = "<leader>e"
+    vim.g.winresizer_vert_resize = 1
+    vim.g.winresizer_horiz_resize = 1
   end
 }
