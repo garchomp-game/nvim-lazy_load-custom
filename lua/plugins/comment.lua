8f580ed27c16e1054dcc9c24ce591397016a50eb
garchomp-game <otoka.japanesedrum@gmail.com>
2024-09-16 08:54:09 +0900

    commit

--- lua/plugins/comment.lua
+++ lua/plugins/comment.lua
@@ -3,14 +3,3 @@ return {
   event = "VeryLazy",
   config = true
 }
-
--- 以下の書き方もいいなーと思って検討中。
---
--- local M = {}
---
--- table.insert(M, 'numtostr/comment.nvim')
---
--- M.event = "VeryLazy"
--- M.config = true
---
--- return M
