7a1b785bd1bfdf9d6962f4b61efb300c6c4aac4b
garchomp-game <otoka.japanesedrum@gmail.com>
2024-11-01 16:38:32 +0900

    commit

--- lua/config/autocmd.lua
+++ lua/config/autocmd.lua
@@ -3,7 +3,5 @@ vim.api.nvim_create_autocmd("FileType", {
   pattern = { "markdown" },
   callback = function()
     vim.opt_local.number = false
-    -- gitsignsの機能を無効化
-    require('gitsigns').detach()
   end,
 })
--- lua/plugins/gitsigns.lua
+++ lua/plugins/gitsigns.lua
@@ -1,28 +1,34 @@
 local utils = require('utils')
 
+-- Gitsignsの設定をローカル変数に分割
+local gitsigns_opts = {
+  -- シンボルの定義
+  signs               = {
+    add          = { text = '│' },
+    change       = { text = '│' },
+    delete       = { text = '_' },
+    topdelete    = { text = '‾' },
+    changedelete = { text = '~' },
+    untracked    = { text = '┆' },
+  },
+  signcolumn          = true, -- `:Gitsigns toggle_signs` で切り替え可能
+  numhl               = false, -- `:Gitsigns toggle_numhl` で切り替え可能
+  linehl              = false, -- `:Gitsigns toggle_linehl` で切り替え可能
+  word_diff           = false, -- `:Gitsigns toggle_word_diff` で切り替え可能
+  attach_to_untracked = true,
+  current_line_blame  = false, -- `:Gitsigns toggle_current_line_blame` で切り替え可能
+
+  -- on_attach 関数: バッファがアタッチされたときに呼び出される
+  on_attach           = function(bufnr)
+    return vim.bo[bufnr].filetype ~= "markdown"
+  end,
+}
+
+-- プラグイン設定の返却
 return utils.setup_non_termux_config(function()
   return {
     'lewis6991/gitsigns.nvim',
-    event = { 'BufRead', 'BufNewFile' }, -- BufReadとBufNewFileイベント時に読み込む
-    config = function(_, opts)
-      require('gitsigns').setup(opts)
-    end,
-    opts = {
-      signs = {
-        add          = { text = '│' },
-        change       = { text = '│' },
-        delete       = { text = '_' },
-        topdelete    = { text = '‾' },
-        changedelete = { text = '~' },
-        untracked    = { text = '┆' },
-      },
-      signcolumn = true,  -- Toggle with `:Gitsigns toggle_signs`
-      numhl      = false, -- Toggle with `:Gitsigns toggle_numhl`
-      linehl     = false, -- Toggle with `:Gitsigns toggle_linehl`
-      word_diff  = false, -- Toggle with `:Gitsigns toggle_word_diff`
-      auto_attach = true,
-      attach_to_untracked = true,
-      current_line_blame = false, -- Toggle with `:Gitsigns toggle_current_line_blame`
-    }
+    event = { 'BufRead', 'BufNewFile' },   -- BufRead と BufNewFile イベント時にプラグインを読み込む
+    opts = gitsigns_opts,
   }
 end)
