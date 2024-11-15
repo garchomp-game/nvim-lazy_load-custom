96f71d0dede8071eeaeb8ac2625ddfa824af3c26
garchomp-game <otoka.japanesedrum@gmail.com>
2024-11-15 11:05:14 +0900

    commit

--- lua/config/mappings.lua
+++ lua/config/mappings.lua
@@ -57,3 +57,12 @@ vim.keymap.set('v', '=', function()
   -- ビジュアルモードを終了
   vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("<esc>", true, false, true), "n", true)
 end, opts)
+
+
+vim.api.nvim_set_keymap("n", "<F5>", ":lua require'dap'.continue()<CR>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<F10>", ":lua require'dap'.step_over()<CR>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<F11>", ":lua require'dap'.step_into()<CR>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<F12>", ":lua require'dap'.step_out()<CR>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<Leader>b", ":lua require'dap'.toggle_breakpoint()<CR>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<Leader>dr", ":lua require'dap'.repl.open()<CR>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("n", "<Leader>dl", ":lua require'dap'.run_last()<CR>", { noremap = true, silent = true })
--- lua/plugins/lsp/jdtls.lua
+++ lua/plugins/lsp/jdtls.lua
@@ -1,35 +1,24 @@
 -- plugins/lsp/jdtls.lua
-
 local M = {}
 
 function M.custom_server_opts(opts)
-  local home = vim.loop.os_homedir()
   local data_path = vim.fn.stdpath("data")
   local workspace_path = data_path .. "/jdtls-workspace/"
   local project_name = vim.fn.fnamemodify(vim.fn.getcwd(), ':p:h:t')
   local lombok_path = data_path .. "/mason/packages/jdtls/lombok.jar"
-  
+
   opts = {
-    -- メイン設定
     cmd = {
       'jdtls',
-      '--jvm-arg=-javaagent:' .. lombok_path,  -- Lombokの設定
-      '--jvm-arg=-Xbootclasspath/a:' .. lombok_path,  -- クラスパスにLombokを追加
+      '--jvm-arg=-javaagent:' .. lombok_path,        -- Lombokの設定
+      '--jvm-arg=-Xbootclasspath/a:' .. lombok_path, -- クラスパスにLombokを追加
       '-data', workspace_path .. project_name
     },
-    
-    -- 言語サーバーの設定
     settings = {
       java = {
         configuration = {
           updateBuildConfiguration = "automatic",
-          -- Lombokのサポートを有効化
-          runtimes = {
-            {
-              name = "JavaSE-11",
-              path = home .. "/.sdkman/candidates/java/11.0.x",
-            }
-          }
+          -- runtimes セクションを削除してデフォルトのJavaランタイムを使用
         },
         maven = {
           downloadSources = true,
@@ -40,7 +29,6 @@ function M.custom_server_opts(opts)
         referencesCodeLens = {
           enabled = true,
         },
-        -- コード生成の設定
         sources = {
           organizeImports = {
             starThreshold = 9999,
@@ -57,20 +45,16 @@ function M.custom_server_opts(opts)
         },
       },
     },
-    
-    -- 初期化オプション
     init_options = {
       bundles = {},
       extendedClientCapabilities = {
         progressReportProvider = true,
-        classFileContentsSupport = true  -- クラスファイルの内容サポート
+        classFileContentsSupport = true -- クラスファイルの内容サポート
       }
     },
-
-    -- シングルファイルのサポート
     single_file_support = true,
   }
-  
+
   return opts
 end
 
